4f7ed31c6454475da4041c044db11641b1400365
afrahulfadilahcantik <1910031802019@sar.ac.id>
2022-06-30 21:30:28 +0700

    first commit

---
+++ app/src/main/java/com/mobile/moviecatalogue/MovieAdapter.kt
@@ -0,0 +1,44 @@
+package com.mobile.moviecatalogue
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.movie_item.view.*
+import kotlinx.android.synthetic.main.movie_item.view.movie_poster
+import kotlinx.android.synthetic.main.movie_item.view.movie_release_date
+import kotlinx.android.synthetic.main.movie_item.view.movie_title
+
+class MovieAdapter(
+
+    private val movies : List<Movie>
+) : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>(){
+
+
+    class MovieViewHolder(view : View) : RecyclerView.ViewHolder(view){
+        private val IMAGE_BASE = "https://image.tmdb.org/t/p/w500/"
+        fun bindMovie(movie : Movie){
+
+            itemView.movie_title.text = movie.title
+            itemView.movie_release_date.text = movie.release
+            itemView.movie_vote_count.text = movie.vote_count
+            itemView.movie_overview.text = movie.overview
+            Glide.with(itemView).load(IMAGE_BASE + movie.poster).into(itemView.movie_poster)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        return MovieViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.movie_item, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int = movies.size
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.bindMovie(movies.get(position))
+    }
+}
+
